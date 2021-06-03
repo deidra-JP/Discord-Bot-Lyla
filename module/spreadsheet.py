ceed4a85efc7af70b92371d0d69c20d8027bcc9e
deidra-JP <deidra@outlook.jp>
2021-06-03 12:12:36 +0900

    0_0_4

--- module/spreadsheet.py
+++ module/spreadsheet.py
@@ -20,6 +20,22 @@ sh = gc.open("Lylaのスプレッドシート")
 
 
 
+def monthcheck():
+    worksheet_feed = sh.worksheets()              
+    today = datetime.date.today().strftime('%Y%m')    
+    exist = False
+    for current in worksheet_feed:
+        if current.title == today:
+            exist = True                                
+    if exist == False:                                 
+         sh.add_worksheet(title=today, rows = 100, cols = 4)      
+         newsheet = sh.worksheet(today) 
+         newsheet.update('A1','収入')
+         newsheet.update('C1','支出')   
+    return sh.worksheet(today)
+
 async def ledger(message):
     if message.content.endswith("スプシ"):
-        print(sh.sheet1.get("A1"))
+        monthcheck()
+        today = datetime.date.today().strftime('%Y%m')
+        print(sh.worksheet(today).get("A1"))
